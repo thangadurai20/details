798f94041f520df4de5f4219c09f00e2f3ca7a3c
thangadurai20 <thangadurai.ee21@bitsathy.ac.in>
2025-02-21 15:29:46 +0530

    assignment_various_format

---
+++ day1/vformats.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+
+printf("decimal: %d\n",n);
+printf("octul: %o\n",n);
+printf("hexadecimal: %x\n",n);
+
+printf("signed input: %i\n",n);
+
+printf("unsigned input: %u\n",n);
+
+}
